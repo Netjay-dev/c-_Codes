4661b6ec29f8c207318ba4442d8b6ef2cde48994
jayson Salatan <jeffreypicar292@gmail.com>
2021-04-19 12:41:41 +0800

    newone

--- A.cpp
+++ A.cpp
@@ -12,103 +12,104 @@
 #define fr(k,n) for(int i = k; i < n; i++) 
 
 using namespace std;
-int n,m;
-bool vis[12][12];
-
-vector<string>ans;
 
 struct trie
 {
     bool isleaf;
-    trie *chd[26];
+    trie *child[26];
+    trie() : isleaf(false)
+    {
+        for(int i = 0; i < 26; ++i)
+        {
+            child[i] = nullptr;
+        }
+    }
 };
 
-trie* create()
-{
-    trie* node = new trie;
-    node->isleaf = false;
-    for(int i = 0; i < 26; ++i)
-        node->chd[i] = nullptr;
-    return node;
-}
-
+unordered_map<string,bool>rf;
 void insert(trie*&head,string s)
 {
-    if(head==nullptr)
-        head = create();
-    trie *cur = head;
+    if(head == nullptr)
+    {
+        head = new trie();
+    }
+    trie*cur = head;
     for(char c : s)
     {
-        if(cur->chd[c-'a']==nullptr)
-            cur->chd[c-'a'] = create();
-        cur = cur->chd[c-'a'];
+        if(cur->child[c-'a']==nullptr)
+        {
+            cur->child[c-'a'] = new trie();
+        }
+        cur = cur->child[c-'a'];
     }
     cur->isleaf = true;
 }
 
-void dfs(trie*head,vector<vector<char>>&board, string s, int row, int col)
+void search(trie*head,vector<vector<bool>>vis, vector<string>&ans,vector<vector<char>>&board,int row,int col,string s)
 {
-    if(row>n || row < 0 || col > m || col < 0 || vis[row][col] || head->chd[board[row][col]-'a']==nullptr){
-        return;
+    int n = board.size(), m = board[0].size();
+    if(head->child[board[row][col]-'a']!=nullptr)
+    {
+        s.push_back(board[row][col]);
+        if(head->child[board[row][col]-'a']->isleaf)
+        {
+            ans.push_back(s);
+        }
+        vis[row][col] = 1;
+        if(col+1<m&&!vis[row][col+1]) search(head->child[board[row][col]-'a'],vis,ans,board,row,col+1,s);
+        if(row+1<n&&!vis[row+1][col]) search(head->child[board[row][col]-'a'],vis,ans,board,row+1,col,s);
+        if(col-1>0&&!vis[row][col-1]) search(head->child[board[row][col]-'a'],vis,ans,board,row,col-1,s);
+        if(row-1>0&&!vis[row-1][col]) search(head->child[board[row][col]-'a'],vis,ans,board,row-1,col,s);
     }
-    s.push_back(board[row][col]);
-    vis[row][col] = 1;
-    if(head->chd[board[row][col]-'a']->isleaf) ans.push_back(s);
-    head = head->chd[board[row][col]-'a'];
-    dfs(head,board,s,row+1,col);
-    dfs(head,board,s,row-1,col);
-    dfs(head,board,s,row,col+1);
-    dfs(head,board,s,row,col-1);
 }
+
 vector<string> findWords(vector<vector<char>>&board,vector<string>&words)
 {
-    n = board.size();
-    m = board[0].size();
-    
-    trie* head = nullptr;
-    for(string &s : words)
-    {
-        insert(head,s);
-    }
+    trie *head = nullptr;
+    int n = board.size(), m = board[0].size();
 
+    for(string s: words) insert(head,s);
+    vector<string>ans;
+
+    trie *cur = head;
     for(int row = 0; row < n; ++row)
     {
         for(int col = 0; col < m; ++col)
         {
-            memset(vis,false,sizeof(vis));
-            dfs(head,board,"",row,col);
+            if(cur->child[board[row][col]-'a']!=nullptr)
+            {
+                vector<vector<bool>>vis(n,vector<bool>(m,false));
+                string s = "";
+                search(cur,vis,ans,board,row,col,s);
+            }
         }
-        cout << endl;
     }
     return ans;
 }
 
 int main()
 {
-    fast_io;
+    ios::sync_with_stdio(false);
     cin.tie(0);
 
-    int a, b;
-    cin >> a >> b;
-    vector<vector<char>>board(a,vector<char>(b));
-
-    for(int row = 0; row < a; ++row)
+    int n, m;
+    cin >> n >> m;
+    vector<vector<char>>board(n,vector<char>(m));
+    for(int row = 0; row < n; ++row)
     {
-        for(int col = 0; col < b; ++col)
+        for(int col = 0; col < m; ++col)
         {
             cin >> board[row][col];
         }
     }
-    
+
     int k;
     cin >> k;
     vector<string>words(k);
-    for(int i = 0; i < k; ++i)
-    {
-        cin >> words[i];
-    }
+    for(string &s : words)
+        cin >> s;
 
-    vector<string>ans = findWords(board,words);
-    for(auto c : ans) cout << c << ' ';
+    vector<string> ans = findWords(board,words);
+
+    for(string &s : ans) cout << s << ' ';
 }
-
